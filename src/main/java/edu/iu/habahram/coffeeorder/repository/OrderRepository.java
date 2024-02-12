67d5a28382e49f68e4e0c30f3ec15b023d85d5cf
jamundy <jamundy@iu.edu>
2024-02-12 09:04:10 -0500

    Added condiments + Beverages

--- src/main/java/edu/iu/habahram/coffeeorder/repository/OrderRepository.java
+++ src/main/java/edu/iu/habahram/coffeeorder/repository/OrderRepository.java
@@ -6,26 +6,23 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class OrderRepository {
     public Receipt add(OrderData order) throws Exception {
-        Beverage beverage = null;
-        switch (order.beverage().toLowerCase()) {
-            case "dark roast":
-                beverage = new DarkRoast();
-                break;
-        }
+        Beverage beverage = switch (order.beverage().toLowerCase()) {
+            case "dark roast" -> new DarkRoast();
+            case "espresso" -> new Espresso();
+            case "house blend" -> new HouseBlend();
+            default -> null;
+        };
         if (beverage == null) {
             throw new Exception("Beverage type '%s' is not valid!".formatted(order.beverage()));
         }
         for(String condiment : order.condiments()) {
-            switch (condiment.toLowerCase()) {
-                case "milk":
-                   beverage = new Milk(beverage);
-                   break;
-                case "mocha":
-                    beverage = new Mocha(beverage);
-                    break;
-                default:
-                    throw new Exception("Condiment type '%s' is not valid".formatted(condiment));
-            }
+            beverage = switch (condiment.toLowerCase()) {
+                case "milk" -> new Milk(beverage);
+                case "mocha" -> new Mocha(beverage);
+                case "soy" -> new Soy(beverage);
+                case "whip" -> new Whip(beverage);
+                default -> throw new Exception("Condiment type '%s' is not valid".formatted(condiment));
+            };
         }
         Receipt receipt = new Receipt(beverage.getDescription(), beverage.cost());
         return receipt;
